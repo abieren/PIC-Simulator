16fd3c25b4ef0439ca2602a0188bcd78ffdfce3b
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2017-05-10 17:27:38 +0200

    added methods

--- PIC_Simulator/src/pic_simulator/model/Port.java
+++ PIC_Simulator/src/pic_simulator/model/Port.java
@@ -12,15 +12,18 @@ package pic_simulator.model;
 public class Port {
     
     private int _latch;  //saves state internal pin latches
-    private int _output; //saves ouput state of pins
+    private int _inout; //saves input/ouput state of pins
     private int _tris;   //saves tris state that determines which pins are output or input pins
+    private int _environment; //saves the state of the environment that tries to set the port
     
     private void updateOutput() {
         //output of pins depend on the tris bits
         //tris=0 means pin is output, tris=1 means bit is input
         //create mask that results the right output if it is combined with latch
         //during an AND operation
-        _output = _latch & (~_tris);
+        _inout = 0;
+        _inout = _inout | (~_tris) & _latch;
+        _inout = _inout | _tris & _environment;
     }
 
     public Port() {
@@ -36,6 +39,7 @@ public class Port {
     }
     
     public void setLatch(int value) {
+        _latch = value;
         updateOutput();
     }
     
@@ -43,16 +47,26 @@ public class Port {
         return _latch;
     }
     
-    public int getOutput() {
-        return _output;
-    }
-    
     public void setTris(int value) {
+        _tris = value;
         updateOutput();
-        
     }
     
     public int getTris() {
         return _tris;
-    }    
+    }  
+    
+    public void setEnvironment(int value) {
+        _environment = value;
+        updateOutput();
+    }
+    
+    public int getEnvironment() {
+        return _environment;
+    }
+    
+    public int getInOut() {
+        return _inout;
+    }
+    
 }
