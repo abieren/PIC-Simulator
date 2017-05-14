1d52c127e182c1c84edce2c34c4b75463d2eb471
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2017-05-14 14:21:44 +0200

    moved interface into package interfaces

---
+++ PIC_Simulator/src/pic_simulator/interfaces/Notifier.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pic_simulator.interfaces;
+
+/**
+ *
+ * @author Alex
+ */
+public interface Notifier {
+    
+    void changedWRegister(int oldValue, int newValue);
+
+    void changedPCRegister(int oldValue, int newValue);
+
+    void changedInstructionRegister(int oldValue, int newValue);
+    
+    public void changedINTCONRegister(int oldValue, int newValue);
+
+    public void changedOPTIONRegister(int oldValue, int newValue);
+    
+    public void changedPortALatch(int oldValue, int newValue);
+
+    public void changedPortAInOut(int oldValue, int newValue);
+
+    public void changedPortATris(int oldValue, int newValue);
+    
+    public void changedPortAEnvironment(int oldEnv, int newValue);
+
+    public void changedPortBLatch(int oldValue, int newValue);
+
+    public void changedPortBTris(int oldValue, int newValue);
+
+    public void changedPortBInOut(int oldValue, int newValue);
+    
+    public void changedPortBEnvironment(int oldEnv, int newValue);
+
+    void changedRegister(int register, int oldValue, int newValue);
+
+    void nextCycle();
+
+    public void popStack(int popedValue, boolean isUnderflow);
+
+    public void pushStack(int pushedValue, boolean isOverflow);
+
+    public void invalidInstruction(int instruction);
+
+    public void changedSTATUSRegister(int statusRegister, int value);
+
+    public void changedFSRRegister(int register, int value);
+    
+}
