6afea3596779aa5a8e8d2ffc4105c61f74fe19a5
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2017-05-14 14:21:56 +0200

    defined interface for timer

---
+++ PIC_Simulator/src/pic_simulator/interfaces/Timer.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pic_simulator.interfaces;
+
+/**
+ *
+ * @author Alex
+ */
+public interface Timer {
+    
+    public void clear();
+    public boolean hasTriggered();
+    public void setPrescalerAssignment(boolean PSAbit);
+    public void setPrescalerRate(boolean PS0bit, boolean PS1bit, boolean PS2bit);
+    public void setWorkingMode(boolean isWorkingAsTimer);
+    public void setIncrementingEdgeMode(boolean incrementOnFallingEdge);
+    public void notifyEdge(boolean isFallingEdge);
+    public void notifyCycle();
+    
+    public void setTimerCount(int value);
+    public int getTimerCount();
+    
+}
