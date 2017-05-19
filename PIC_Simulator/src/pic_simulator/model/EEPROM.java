b650a780163c3a221b7c9ff78b4e64f6ff1a0b0d
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2017-05-19 12:34:15 +0200

    added EEPROM class

---
+++ PIC_Simulator/src/pic_simulator/model/EEPROM.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pic_simulator.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import pic_simulator.utils.BinaryNumberHelper;
+
+/**
+ *
+ * @author Alex
+ */
+public class EEPROM {
+    
+    private static final int MAX_ADDRESS = 0x3F;
+    private static final int DEFAULT_VALUE = 0x3F;
+    private Map<Integer, Integer> _registers = new HashMap<>();
+    
+    private int shrinkAddress(int address) {
+        return BinaryNumberHelper.truncateToNBit(address, 6);
+    }
+    
+    public void setRegsiter(int address, int value) {
+        address = shrinkAddress(address);
+        value = BinaryNumberHelper.truncateToNBit(value, 8);
+        _registers.put(address, value);
+    };
+    
+    public int getRegsiter(int address) {
+        address = shrinkAddress(address);
+        Integer result = _registers.get(address);
+        if (result == null) {
+            setRegsiter(address, DEFAULT_VALUE);
+            return DEFAULT_VALUE;
+        }
+        return result;
+    }
+    
+}
